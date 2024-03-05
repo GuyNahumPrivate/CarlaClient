5fde15046f855e9ebe15f1453ddb5f4acbebf030
GuyNahumPrivate <w00tiownguy@gmail.com>
2024-03-05 20:53:22 +0200

    Print path distance on astar_graph_searcher.py

--- GraphSearcher/astar_graph_searcher.py
+++ GraphSearcher/astar_graph_searcher.py
@@ -4,4 +4,17 @@ import networkx as nx
 
 class AStarGraphSearcher(GraphSearcher):
     def find_path(self, graph, source, target, heuristic, weight='length'):
-        return nx.astar_path(graph, source, target, heuristic, weight)
+        path = nx.astar_path(graph, source, target, heuristic, weight)
+        total_distance = 0
+        for i in range(len(path) - 1):
+            # Get the length of the edge between consecutive nodes in the path
+            if isinstance(weight, str):
+                edge_length = graph[path[i]][path[i + 1]][weight]
+            else:
+                edge_length = weight(path[i], path[i + 1], graph[path[i]][path[i + 1]])
+
+            total_distance += edge_length
+
+        print("Total distance of the path:", total_distance)
+
+        return path
